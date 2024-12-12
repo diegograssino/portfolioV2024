0537bfd0c3fad7440a11a1f5380c95fa18406359
diegograssino <diegograssino@gmail.com>
2024-12-12 20:45:13 -0300

    Light refactor and various UI fixes on navbar

--- components/shared/Navbar/index.tsx
+++ components/shared/Navbar/index.tsx
@@ -2,7 +2,7 @@
 import { sections } from "@/content/sections";
 import { SectionContext } from "@/contexts/section";
 import { animations } from "@/utils/animations";
-import { IconCircleFilled, IconMenu2, IconX } from "@tabler/icons-react";
+import { IconArrowRight, IconMenu2, IconX } from "@tabler/icons-react";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
@@ -10,7 +10,7 @@ import Container from "../Container";
 import ToggleTheme from "../ToggleTheme";
 
 const Navbar = () => {
-  const { inViewSection } = useContext(SectionContext)!;
+  const { inViewSection, setInViewSection } = useContext(SectionContext)!;
   const [isMenuVisible, setIsMenuVisible] = useState(false);
   const [scrolling, setScrolling] = useState(false);
 
@@ -35,11 +35,12 @@ const Navbar = () => {
   function scrollToTop() {
     if (!isBrowser()) return;
     window.scrollTo({ top: 0, behavior: "smooth" });
+    setInViewSection("");
   }
 
   return (
     <nav
-      className={`text-white py-2 sticky top-0 h-[2.5rem] z-30 transition ease-in-out duration-700 ${
+      className={`text-white py-2 sticky top-0 z-30 transition ease-in-out duration-700 ${
         scrolling
           ? "bg-secondary-lighter dark:bg-secondary-darker"
           : "bg-transparent"
@@ -54,20 +55,23 @@ const Navbar = () => {
           transition={{ duration: 0.7, delay: 0.6 }}
           className="grid grid-cols-2 md:grid-cols-3"
         >
-          {/* TODO On mobile navbar view, on click close the offcanvas */}
           <div
             className="flex self-center flex-row gap-1 z-20 cursor-pointer"
             onClick={scrollToTop}
           >
-            <span className="font-code text-xs lg:text-base text-tertiary-lighter sm:text-white">{`<`}</span>
-            <span
-              className={`hidden sm:block font-code text-xs lg:text-base font-bold transition ease-in-out duration-700 ${
-                scrolling
-                  ? "text-tertiary-darker dark:text-tertiary-lighter"
-                  : "text-white"
-              }`}
-            >{`MyPortfolio`}</span>
-            <span className="font-code text-xs lg:text-base text-tertiary-lighter sm:text-white">{` />`}</span>
+            {!isMenuVisible ? (
+              <>
+                <span className="font-code text-xs lg:text-base text-tertiary-lighter sm:text-white">{`<`}</span>
+                <span
+                  className={`hidden sm:block font-code text-xs lg:text-base font-bold transition ease-in-out duration-700 ${
+                    scrolling
+                      ? "text-tertiary-darker dark:text-tertiary-lighter"
+                      : "text-white"
+                  }`}
+                >{`MyPortfolio`}</span>
+                <span className="font-code text-xs lg:text-base text-tertiary-lighter sm:text-white">{` />`}</span>
+              </>
+            ) : null}
           </div>
           {/* TODO refactor navbar to show only desktop or mobile version, not using display none */}
           <div className="hidden md:block">
@@ -89,16 +93,9 @@ const Navbar = () => {
               ))}
             </ul>
           </div>
-          <div className="flex justify-end gap-3 md:gap-5">
+          <div className="flex justify-end gap-5">
             {/* TODO Add multi language toogler */}
-            {/* <button className="z-20">
-              <IconSearch
-                className="flex self-center dark:opacity-75 hover:opacity-75 transition duration-200 ease-in-out"
-                size={"1.2rem"}
-              />
-            </button> */}
-            {/* TODO Add a B&W theme if scrolling */}
-            <ToggleTheme isVisible={scrolling} />
+            <ToggleTheme isVisible={scrolling && !isMenuVisible} />
             <button className="md:hidden z-20">
               {!isMenuVisible ? (
                 <IconMenu2
@@ -128,16 +125,16 @@ const Navbar = () => {
             transition={{ y: { velocity: 100 }, duration: 0.2 }}
             className={`absolute inset-y-0 right-0 w-full h-screen md:h-auto bg-secondary pt-16 pb-8 z-0 md:hidden`}
           >
-            <Container as="ul">
+            <Container as="ul" className="flex flex-col gap-3 pl-3">
               {sections.map((section, i) => (
                 <li key={i} className="flex gap-2">
-                  <IconCircleFilled
+                  <IconArrowRight
                     className="self-center ml-1 text-tertiary"
-                    size={16}
+                    size={32}
                   />
                   <Link
                     href={`#${section.slug}`}
-                    className="text-[1.5rem] hover:font-bold"
+                    className="text-[2rem] md:hover:font-bold"
                     onClick={() => handleMenuVisibility(false)}
                   >
                     {section.name}
