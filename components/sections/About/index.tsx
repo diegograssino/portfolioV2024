33c439407d375ee311b373b627ea9a6a772961d4
diegograssino <diegograssino@gmail.com>
2024-12-15 22:12:58 -0300

    Add mdx support and minimun content

--- components/sections/About/index.tsx
+++ components/sections/About/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import Content from "./about.mdx";
 const About = () => {
   return (
-    <section className="min-h-[calc(100vh)] pt-12" id="about">
-      <h2>About</h2>
+    <section
+      className="min-h-[calc(100vh)] pt-12 flex flex-col gap-4"
+      id="about"
+    >
+      <Content />
     </section>
   );
 };
